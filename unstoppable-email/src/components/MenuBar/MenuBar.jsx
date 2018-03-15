699bdf9c5570a5bc263bcb879b6488ea9ae94757
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2018-03-15 10:36:04 -0400

    added menu bar component

---
+++ unstoppable-email/src/components/MenuBar/MenuBar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default connect()(class MenuBar extends Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+});
