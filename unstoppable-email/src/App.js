61082dd72dd8781427c3c70af3dc7207b9715cbb
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2018-03-15 10:59:15 -0400

    added redux dev tools

--- unstoppable-email/src/App.js
+++ unstoppable-email/src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import Root from './Root';
 import eth from './reducers/eth';
 
+/* eslint-disable no-underscore-dangle */
 let store = createStore(combineReducers({
-  eth
-}), applyMiddleware(thunk));
+  eth,
+  form: formReducer
+}), 
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+, applyMiddleware(thunk));
+/* eslint-enable */
 
 class App extends Component {
   render() {
